117db9ba3c54eaaa6767ef5cb2ff42323ed78d34
David Fang <fang@csl.cornell.edu>
2014-03-07 06:54:03 -0800

    Revert "downgrade libstdc++-4.7 check to warning"
    This reverts commit 607f7296a940fe90f54a41d4d9d3600bf9e89c36.

--- cmake/modules/HandleLLVMOptions.cmake
+++ cmake/modules/HandleLLVMOptions.cmake
@@ -34,7 +34,7 @@ std::atomic<float> x(0.0f);
 int main() { return (float)x; }"
         LLVM_NO_OLD_LIBSTDCXX)
       if(NOT LLVM_NO_OLD_LIBSTDCXX)
-        message(WARNING "Host Clang must be able to find libstdc++4.7 or newer! (unless you have the right patches)")
+        message(FATAL_ERROR "Host Clang must be able to find libstdc++4.7 or newer!")
       endif()
       set(CMAKE_REQUIRED_FLAGS ${OLD_CMAKE_REQUIRED_FLAGS})
       set(CMAKE_REQUIRED_LIBRARIES ${OLD_CMAKE_REQUIRED_LIBRARIES})
