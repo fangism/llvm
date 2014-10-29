c6c1da9189e898a4ad944291efa49131bd3fa0c1
David Fang <fang@csl.cornell.edu>
2014-10-28 17:08:39 -0700

    patch from Iain Sandoe to fix PR14579 (missing llvm test case)

--- lib/Target/PowerPC/PPCSubtarget.cpp
+++ lib/Target/PowerPC/PPCSubtarget.cpp
@@ -156,7 +156,8 @@ bool PPCSubtarget::hasLazyResolverStub(const GlobalValue *GV,
     return false;
   // If symbol visibility is hidden, the extra load is not needed if
   // the symbol is definitely defined in the current translation unit.
-  bool isDecl = GV->isDeclaration() && !GV->isMaterializable();
+  bool isDecl = (GV->isDeclaration() && !GV->isMaterializable()) ||
+		GV->hasAvailableExternallyLinkage();	// fixes PR14579
   if (GV->hasHiddenVisibility() && !isDecl && !GV->hasCommonLinkage())
     return false;
   return GV->hasWeakLinkage() || GV->hasLinkOnceLinkage() ||
